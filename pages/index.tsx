9ba270f15b81a5f5833f00da3c911cc7ebb4cd91
Naufal Fikri <naufal@naufik.net>
2023-03-02 22:10:06 +1100

    Stylesheet and layout fix.

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,28 +9,52 @@ const Home: NextPage = () => {
       <Head>
         <title>Naufal Fikri</title>
       </Head>
-       <main className={styles.main}>
-         <div className={styles.imageContainer}>
-           <img className={styles.splashImg} src="/me.png" />
-         </div>
+      <main className={styles.main}>
+        <div className={styles.imageContainer}>
+          <img className={styles.splashImg} src="/me.png" />
+        </div>
         <div className={styles.splashContent}>
-          <h1>
-            Naufal Fikri
-          </h1>
+          <h1>Naufal Fikri</h1>
           <p>Hello there,</p>
           <p>
-            I&apos;m a software engineer and former computer science educator based in Melbourne, Australia. I don&apos;t have a specialization, but I enjoy work around back-end development, dev-ops, and automation. 
+            I&apos;m a software engineer and former computer science educator
+            based in Melbourne, Australia. I don&apos;t have a specialization,
+            but I enjoy work around back-end development, dev-ops, and
+            automation.
           </p>
           <p>
             <span className={styles.section}>Work history</span>:
             <ul>
               <li>
-                <strong><a href="https://ingenious.ai">InGenious AI</a></strong>, Full Stack Software Engineer (2020-): <br/>Building a platform
-                to craft engaging and feature-rich chatbots.
+                <strong>
+                  <a href="https://ingenious.ai">InGenious AI</a>
+                </strong>
+                , Full Stack Software Engineer (2020-): <br />
+                Building a platform to craft engaging and feature-rich chatbots.
               </li>
-              <li><strong><a href="https://cis.unimelb.edu.au">University of Melbourne</a></strong>, Teaching Assistant (2018-2020)</li>
-              <li><strong>University of Melbourne</strong>, Research Assistant (2019): <br/>Published <a href="http://renata.borovica-gajic.com/data/2020_adc.pdf">Function Interpolation for Learned Index Structures</a>.</li>
-              <li><strong><a href="https://nodeflux.io">Nodeflux</a></strong>, Software Engineer Intern (2018).</li>
+              <li>
+                <strong>
+                  <a href="https://cis.unimelb.edu.au">
+                    University of Melbourne
+                  </a>
+                </strong>
+                , Teaching Assistant (2018-2020)
+              </li>
+              <li>
+                <strong>University of Melbourne</strong>, Research Assistant
+                (2019): <br />
+                Published{" "}
+                <a href="http://renata.borovica-gajic.com/data/2020_adc.pdf">
+                  Function Interpolation for Learned Index Structures
+                </a>
+                .
+              </li>
+              <li>
+                <strong>
+                  <a href="https://nodeflux.io">Nodeflux</a>
+                </strong>
+                , Software Engineer Intern (2018).
+              </li>
             </ul>
           </p>
           <span className={styles.navTemp}>
@@ -40,9 +64,9 @@ const Home: NextPage = () => {
             {" | "}
             <a href="https://linkedin.com/in/naufik">LinkedIn</a>
           </span>
-        <footer className={styles.footer}>
-          <p>&copy; Naufal Fikri Setiawan 2016-2023</p>
-        </footer>
+          <footer className={styles.footer}>
+            <p>&copy; Naufal Fikri Setiawan 2016-2023</p>
+          </footer>
         </div>
       </main>
     </div>
